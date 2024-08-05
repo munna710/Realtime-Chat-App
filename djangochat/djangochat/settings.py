8d95c08805fa29225fd32b6a3b7fea8fc3ebe651
Munna710 <munna7777777.1.0@gmail.com>
2024-08-05 23:31:04 +0530

    chore: Add initial project files and structure

--- djangochat/djangochat/settings.py
+++ djangochat/djangochat/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chat',
+    'bootstrap5',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'djangochat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
