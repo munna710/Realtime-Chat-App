8f957a6eb381e92edb1c519365b8e60e9b65e8c0
Munna710 <munna7777777.1.0@gmail.com>
2024-08-06 17:31:21 +0530

    chore: Add ChatConsumer class for handling WebSocket connections

--- djangochat/djangochat/asgi.py
+++ djangochat/djangochat/asgi.py
@@ -8,9 +8,23 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
 
-application = get_asgi_application()
+from chat import routing
+
+django_asgi_application = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        'http': django_asgi_application,
+        'websocket': AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+        )
+    }
+)
