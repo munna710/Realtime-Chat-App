ecb0a7104615e0f9b4f01c369a07387f0adda2f4
Munna710 <munna7777777.1.0@gmail.com>
2024-08-08 19:56:05 +0530

    chore: Add domain to CSRF_TRUSTED_ORIGINS in Django settings

--- djangochat/djangochat/settings.py
+++ djangochat/djangochat/settings.py
@@ -35,6 +35,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+# Add your domain to CSRF_TRUSTED_ORIGINS
+CSRF_TRUSTED_ORIGINS = ['https://realtime-chat-app-8oy9.onrender.com']
+
 # RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 # if RENDER_EXTERNAL_HOSTNAME:
 #     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
