5459b9e7d53c3ff56ed3be17b9f07455c3b83ed3
Munna710 <munna7777777.1.0@gmail.com>
2024-08-08 17:26:49 +0530

    chore: Update Django setup in ASGI file

--- djangochat/djangochat/asgi.py
+++ djangochat/djangochat/asgi.py
@@ -12,9 +12,10 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
+django.setup()
 
 from chat import routing
 
